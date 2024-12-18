a4a6bb17d7331775ad84b4591d81af8f62ed6fbe
Maeyama Wataru <24aw0134@jec.ac.jp>
2024-12-19 03:07:06 +0900

    本当のとりあえず完成

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,7 +2,7 @@
 
 // ハンバーガーメニューの実装
 const navBtn = document.querySelector(".header__hamburger-btn");
-const navList = document.querySelector("nav");
+const navList = document.querySelector(".header__hamburger-nav");
 navBtn.addEventListener("click", function(){
     navList.classList.toggle("open");
     document.querySelector(".header__hamburger-btn__line01").classList.toggle("open");
@@ -11,15 +11,44 @@ navBtn.addEventListener("click", function(){
 });
 
 
+// ハンバーガーメニューのリンクをクリックしたらハンバーガーメニューを閉じる
+const links = document.querySelectorAll(".header__hamburger-nav a");
+console.log(links);
+links.forEach(link => {
+    link.addEventListener("click", function(){
+        navList.classList.toggle("open");
+        document.querySelector(".header__hamburger-btn__line01").classList.toggle("open");
+        document.querySelector(".header__hamburger-btn__line02").classList.toggle("open");
+        document.querySelector(".header__hamburger-btn__line03").classList.toggle("open");
+    });    
+});
 
 
-// ハンバーガーメニューのリンクをクリックしたらハンバーガーメニューを閉じる(未完)
-// const link = document.querySelectorAll(".header__hamburger-nav a");
-// // マップメソッドか何かで、配列の中のそれぞれにイベントを仕組んでいきたい。
-// console.log(link);
-// link.addEventListener("click", function(){
-//     navList.classList.toggle("open");
-//     document.querySelector(".header__hamburger-btn__line01").classList.toggle("open");
-//     document.querySelector(".header__hamburger-btn__line02").classList.toggle("open");
-//     document.querySelector(".header__hamburger-btn__line03").classList.toggle("open");
-// });
+// レスポンシブでメニューを切り替える
+const pcMenu = document.querySelector(".container__pc-menu");
+function responsive(){
+    if (window.matchMedia('(max-width: 1120px)').matches) {
+        navBtn.style.display = '';
+        pcMenu.style.display = 'none';
+    } else if (window.matchMedia('(min-width:1120px)').matches) {
+        navBtn.style.display = 'none';
+        pcMenu.style.display = '';
+    }    
+}
+
+
+// パソコン版メニューバーの実装
+const about_zionmeet = document.querySelector(".deployment");
+const pcMenu_h2 = document.querySelector(".pc-menu__h2");
+about_zionmeet.addEventListener("mouseenter", (e) => {
+    pcMenu_h2.classList.add("open");
+});
+pcMenu_h2.addEventListener("mouseleave", (e) => {
+    pcMenu_h2.classList.remove("open");
+});
+
+
+
+window.addEventListener('resize', responsive);
+
+responsive();
