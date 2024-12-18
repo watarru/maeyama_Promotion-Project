8b51eeec587e4836843b66a66353b1336c17e66c
Maeyama Wataru <24aw0134@jec.ac.jp>
2024-12-18 17:51:08 +0900

    レスポンシブ完成 後はPCのメニューバーのみ

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -10,12 +10,16 @@ navBtn.addEventListener("click", function(){
     document.querySelector(".header__hamburger-btn__line03").classList.toggle("open");
 });
 
-const link = document.querySelectorAll(".header__hamburger-nav a");
-// マップメソッドか何かで、配列の中のそれぞれにイベントを仕組んでいきたい。
-console.log(link);
-link.addEventListener("click", function(){
-    navList.classList.toggle("open");
-    document.querySelector(".header__hamburger-btn__line01").classList.toggle("open");
-    document.querySelector(".header__hamburger-btn__line02").classList.toggle("open");
-    document.querySelector(".header__hamburger-btn__line03").classList.toggle("open");
-});
+
+
+
+// ハンバーガーメニューのリンクをクリックしたらハンバーガーメニューを閉じる(未完)
+// const link = document.querySelectorAll(".header__hamburger-nav a");
+// // マップメソッドか何かで、配列の中のそれぞれにイベントを仕組んでいきたい。
+// console.log(link);
+// link.addEventListener("click", function(){
+//     navList.classList.toggle("open");
+//     document.querySelector(".header__hamburger-btn__line01").classList.toggle("open");
+//     document.querySelector(".header__hamburger-btn__line02").classList.toggle("open");
+//     document.querySelector(".header__hamburger-btn__line03").classList.toggle("open");
+// });
